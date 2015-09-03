96278b2bd7b9003b472d95410d790e30963c115f
Changwan Jun <me@wani.kr>
2015-09-04 02:01:09 +0900

    remove not using files. (test files)

--- tests/Stubs/ArticleCachedRepository.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Wandu\Laravel\Repository\Stubs;
-
-use Wandu\Laravel\Repository\Caching\CachedMoreItemsRepository;
-
-class ArticleCachedRepository extends CachedMoreItemsRepository implements ArticleRepositoryInterface
-{
-}
--- tests/Stubs/UserCachedRepository.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Wandu\Laravel\Repository\Stubs;
-
-use Wandu\Laravel\Repository\Caching\CachedPaginationRepository;
-
-class UserCachedRepository extends CachedPaginationRepository implements UserRepositoryInterface
-{
-}
